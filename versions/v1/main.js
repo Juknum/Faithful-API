15daa7e7b55dedf081071b62373c413af36add1a
Julien Constant <julienconstant190@gmail.com>
2021-11-29 19:20:38 +0100

    added search into collection (only 1 search is currently available)

--- versions/v1/main.js
+++ versions/v1/main.js
@@ -7,12 +7,14 @@ const texture = require('./routes/texture')
 const textures = require('./routes/textures')
 const contribution = require('./routes/contribution')
 const addon = require('./routes/addon')
+const search = require('./routes/search')
 
 router.use('/raw', raw)
 router.use('/texture', texture)
 router.use('/textures', textures)
 router.use('/contribution', contribution)
 router.use('/addon', addon)
+router.use('/search', search)
 
 router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '..', '..', '/page.html'))
