41b5b2e1e7c161379d35238394589205fe3471d4
Julien Constant <julienconstant190@gmail.com>
2022-01-14 11:58:59 +0100

    added dark theme (for my  eyes aha)
    Not the best to do it, but the only that works properly F

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,27 @@
 import express, { Application, Request, Response } from 'express';
+import fs from 'fs';
+import path from 'path';
 import swaggerUi from 'swagger-ui-express';
-
 import Router from './routes';
 
-const PORT = process.env.PORT || 8000;
+(async () => {
+	const PORT = process.env.PORT || 8000;
 
-const app: Application = express();
-app.use(express.static('public'));
+	const app: Application = express();
+	app.use(express.static('public'));
 
-//serves docs
-app.use(
-	'/',
-	swaggerUi.serve,
-	swaggerUi.setup(undefined, {
-		swaggerOptions: {
-			url: '/swagger.json',
-		},
-	}),
-);
+	let css: string = (await fs.readFileSync(path.join(__dirname, '../public/custom.css'))).toString();
 
-app.use(Router); //endpoints
+	//serves docs
+	app.use(
+		'/',
+		swaggerUi.serve, //'', '/swagger.json', 'Compliance API'
+		swaggerUi.setup(require('../public/swagger.json'), undefined, undefined, css, 'https://database.compliancepack.net/images/brand/icons/general/compliance_white.ico', undefined, 'Compliance API'),
+	);
 
-app.listen(PORT, () => {
-	console.log('Server is running on port', PORT);
-});
+	app.use(Router); // endpoints
+
+	app.listen(PORT, () => {
+		console.log(`Server is running at http://localhost:${PORT}`);
+	});
+})();
