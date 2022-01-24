46c4ac981a38d0a88baa46c88903db0783c8d593
TheRolf <yann.lv@live.fr>
2022-01-24 10:58:41 +0100

    API endpoint to get header and screenshot images

--- src/v2/controller/addon.controller.ts
+++ src/v2/controller/addon.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Path, Route, Tags } from "tsoa";
+import { Request as ExRequest, Response as ExResponse } from "express";
+import { Controller, Get, Path, Request, Response, Route, SuccessResponse, Tags } from "tsoa";
 
 import { Addon, Addons, Files, AddonAll } from "../interfaces";
 import AddonService from "../service/addon.service";
+import { NotFoundError, ApiError } from "./../tools/ApiError";
 
 @Route("addons")
 @Tags("Addons")
@@ -28,5 +30,21 @@ export class AddonController extends Controller {
 		return this.service.getFiles(id);
 	}
 
-	// todo: implements setter with authentification verification
+	@Response<ApiError>(404)
+	@Get("{id}/files/screenshots/{index}")
+	@SuccessResponse(302, "Redirect")
+	public async getScreenshot(@Path() id: number, @Path() index: number, @Request() request: ExRequest) {
+		const screenshotURL = await this.service.getSreenshotURL(id, index);
+		const response = (<any>request).res as ExResponse;
+		response.redirect(screenshotURL);
+	}
+
+	@Response<NotFoundError>(404)
+	@Get("{id}/files/header")
+	@SuccessResponse(302, "Redirect")
+	public async getHeaderFile(@Path() id: number, @Request() request: ExRequest): Promise<void> {
+		const headerFileURL = await this.service.getHeaderFileURL(id);
+		const response = (<any>request).res as ExResponse;
+		response.redirect(headerFileURL);
+	}
 }
--- src/v2/service/addon.service.ts
+++ src/v2/service/addon.service.ts
@@ -1,5 +1,6 @@
 import { Addons, Addon, AddonAll, AddonRepository, Files } from "../interfaces";
 import AddonFirestormRepository from "../repository/firestorm/addon.repository";
+import { NotFoundError } from "../tools/ApiError";
 
 export default class AddonService {
 	private readonly addonRepo: AddonRepository = new AddonFirestormRepository();
@@ -27,6 +28,42 @@ export default class AddonService {
 				files: results[1],
 			};
 		});
+	}
+
+	async getSreenshotURL(id: number, index: number): Promise<string> {
+		const files = await this.getFiles(id);
+
+		// if no files, not found
+		if (files === undefined) {
+			throw new NotFoundError("Files not found");
+		}
+
+		const screenshotFile = files.filter((f) => f.use === "screenshot" || (f as any).use === "carousel")[index];
+
+		// if no header file, not found
+		if (screenshotFile === undefined) {
+			throw new NotFoundError("File not found");
+		}
+
+		return screenshotFile.source;
+	}
+
+	async getHeaderFileURL(id: number): Promise<string> {
+		const files = await this.getFiles(id);
+
+		// if no files, not found
+		if (files === undefined) {
+			throw new NotFoundError("Files not found");
+		}
+
+		const headerFile = files.filter((f) => f.use === "header")[0];
+
+		// if no header file, not found
+		if (headerFile === undefined) {
+			throw new NotFoundError("File not found");
+		}
+
+		return headerFile.source;
 	}
 
 	// todo: implements setter with authentification verification
