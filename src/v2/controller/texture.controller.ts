0b0eb09add4223c497db1ce84d08bd83efe839f5
TheRolf <yann.lv@live.fr>
2022-05-17 19:42:32 +0200

    You can now make request with ids splitted by ,

--- src/v2/controller/texture.controller.ts
+++ src/v2/controller/texture.controller.ts
@@ -63,23 +63,34 @@ export class TextureController extends Controller {
 
 	/**
 	 * Get a texture using it's ID
-	 * @param id_or_name Texture ID or texture name
+	 * @param id_or_name Texture ID or texture name, splitted by "," if multiple
 	 */
 	@Get("{id_or_name}")
-	public async getTexture(@Path() id_or_name: string | number): Promise<Textures | Texture> {
+	public async getTexture(@Path() id_or_name: string | number): Promise<Texture | Texture[]> {
+		if(typeof id_or_name === 'string' && id_or_name.includes(',')) {
+			const id_array = id_or_name.split(',');
+			return Promise.allSettled(id_array.map(id => this.service.getByNameOrId(id)))
+				.then(res => res.filter(p => p.status === "fulfilled").map((p: any) => p.value).flat());
+		}
 	  return this.service.getByNameOrId(id_or_name);
 	}
 
 	/**
 	 * Get more information about all textures that have the given string in their name
-	 * @param name Searched texture name
+	 * @param name Searched texture name, splitted by "," if multiple
 	 * @param property Property from the texture
 	 */
 	@Get("{id_or_name}/{property}")
 	public async getTexturesProperty(
 		@Path() id_or_name: string | number,
 		@Path() property: TextureProperty,
-	): Promise<Textures | Texture | Paths | Uses | Contributions> {
+	): Promise<Textures | Texture | Paths | Uses | Contributions | (Textures | Texture | Paths | Uses | Contributions)[]> {
+		if(typeof id_or_name === 'string' && id_or_name.includes(',')) {
+			const id_array = id_or_name.split(',');
+			return (Promise.allSettled(id_array.map(id => this.service.getPropertyByNameOrId(id, property)))
+				.then(res => res.filter(p => p.status === "fulfilled").map((p: any) => p.value).flat())) as Promise<(Textures | Texture | Paths | Uses | Contributions)[]>;
+		}
+		
 	  return this.service.getPropertyByNameOrId(id_or_name, property);
 	}
 
