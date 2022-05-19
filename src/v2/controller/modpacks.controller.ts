08d15a4f910a998b62c002affc386b5473e030fb
Julien Constant <julienconstant190@gmail.com>
2022-05-20 00:17:35 +0200

    added thumbnail endpoints for modpacks (with cache too)

--- src/v2/controller/modpacks.controller.ts
+++ src/v2/controller/modpacks.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Route, Tags } from "tsoa";
+import { Controller, Get, Path, Request, Route, SuccessResponse, Tags } from "tsoa";
+import { Request as ExRequest, Response as ExResponse } from "express";
 import { Modpacks } from "../interfaces";
 import ModpacksService from "../service/modpacks.service";
+import cache from "../tools/cache";
 
 
 @Route("modpacks")
@@ -14,5 +16,13 @@ export class ModpacksController extends Controller {
 	@Get("raw")
 	public async getRaw(): Promise<Modpacks> {
 		return this.service.getRaw();
+	}
+
+	@Get("{id}/thumbnail")
+	@SuccessResponse(302, "Redirect")
+	public async getThumbnail(@Path() id: string, @Request() request: ExRequest): Promise<void> {
+		const url = await cache.handle(`modpacks-thumbnail-${id}`, () => this.service.getThumbnail(parseInt(id, 10)));
+		const response = (<any>request).res as ExResponse;
+		response.redirect(url);
 	}
 }
