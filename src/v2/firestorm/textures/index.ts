a656aa30217336d6b9008d9a132eb63a1c86d348
Robert <contact.e3nk6@aleeas.com>
2022-09-22 17:27:42 +0200

    Revert "Fixed mod textures paths not showing"
    This reverts commit 7c6b31830b7d9b138757ea7df1bf608fde546490.

--- src/v2/firestorm/textures/index.ts
+++ src/v2/firestorm/textures/index.ts
@@ -60,17 +60,7 @@ export const textures = firestorm.collection("textures", (el) => {
 				[use] = _uses.filter((u: Use) => u.id === path.use);
 
 				// TODO: '|| path.name.includes('assets/realms')' is a temp fix,
-				let ret = `${urls[use.edition]}${version}/`
-				if (use.assets === null || path.name.includes('assets/realms') || 
-					['assets/forge', 'assets/fml', 'assets/fabric', 'assets/modmenu']
-						.reduce((_, cur) => cur || path.name.includes(cur), false))
-				{
-					ret += path.name
-				} else {
-					ret += `assets/${use.assets}/${path.name}`
-				}
-
-				return ret
+				return `${urls[use.edition]}${version}/${use.assets === null || path.name.includes('assets/realms') ? path.name : `assets/${use.assets}/${path.name}`}`;
 			})
 			.catch("https://raw.githubusercontent.com/Faithful-Resource-Pack/App/main/resources/transparency.png"); // fallback image
 	};
