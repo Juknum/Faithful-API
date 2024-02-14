5ca050afb2e690046b49da1fdf4236fd29dec9cc
Evorp <3vorpgaming@gmail.com>
2024-02-13 19:22:57 -0800

    camelCase + service

--- src/v2/controller/auth.controller.ts
+++ src/v2/controller/auth.controller.ts
@@ -1,30 +1,11 @@
-import express from "express";
+import { Request as ExRequest, Response as ExResponse } from "express";
 import { Body, Controller, Get, Path, Post, Query, Request, Route, Tags } from "tsoa";
-
-// REDIRECT URL IS '<origin>/auth/<provider>/callback'
-const REDIRECT_URI_PATHNAME = "/callback";
-// example with discord provider:
-// REDIRECT URL IS '<origin>/auth/discord/callback'
+import AuthService from "../service/auth.service";
 
 @Route("auth")
 @Tags("Auth")
 export class AuthController extends Controller {
-	private rawTargetToURL(target: string) {
-		if (target === "webapp") return process.env.WEBAPP_URL;
-		return "";
-	}
-
-	private targetToURL(target: string) {
-		return this.rawTargetToURL(target).replace(/\/$/, "");
-	}
-
-	private shapeRedirectURI(request: express.Request) {
-		const api_origin = `${request.protocol}://${request.headers.host}`;
-		const request_path = request.path;
-		const path_split = request_path.split("/");
-		path_split.pop(); // remove target
-		return api_origin.replace(/\/$/, "") + path_split.join("/") + REDIRECT_URI_PATHNAME;
-	}
+	private readonly service = new AuthService();
 
 	/**
 	 * Handles Discord authorization page redirect
@@ -33,7 +14,7 @@ export class AuthController extends Controller {
 	public async discordAuthCallback(
 		@Query() code: string,
 		@Query() state: string,
-		@Request() request: express.Request,
+		@Request() request: ExRequest,
 	) {
 		const params = new URLSearchParams();
 		params.append("client_id", process.env.DISCORD_CLIENT_ID);
@@ -43,12 +24,13 @@ export class AuthController extends Controller {
 		params.append("redirect_uri", process.env.REDIRECT_DOMAIN.replace(/\/$/, "") + request.path);
 		params.append("scope", "identify");
 
-		const res = (<any>request).res as express.Response;
-		const token_response = await fetch("https://discord.com/api/oauth2/token", {
+		const res = (<any>request).res as ExResponse;
+		const tokenResponse = await fetch("https://discord.com/api/oauth2/token", {
 			method: "POST",
 			body: params,
 		});
-		const json: any = await token_response.json();
+
+		const json: any = await tokenResponse.json();
 
 		if ("error" in json) {
 			res.status(500).json(json).end();
@@ -56,7 +38,7 @@ export class AuthController extends Controller {
 		}
 
 		res.redirect(
-			`${this.targetToURL(state)}/?access_token=${encodeURIComponent(json.access_token)}&refresh_token=${encodeURIComponent(
+			`${this.service.targetToURL(state)}/?access_token=${encodeURIComponent(json.access_token)}&refresh_token=${encodeURIComponent(
 				json.refresh_token,
 			)}&expires_in=${encodeURIComponent(json.expires_in)}`,
 		);
@@ -66,22 +48,19 @@ export class AuthController extends Controller {
 	 * Handles Discord refresh for new sessions
 	 */
 	@Post("discord/refresh")
-	public async discordAuthRefresh(
-		@Body() refresh_token: string,
-		@Request() request: express.Request,
-	) {
+	public async discordAuthRefresh(@Body() refresh_token: string, @Request() request: ExRequest) {
 		const params = new URLSearchParams();
 		params.append("client_id", process.env.DISCORD_CLIENT_ID);
 		params.append("client_secret", process.env.DISCORD_CLIENT_SECRET);
 		params.append("grant_type", "refresh_token");
 		params.append("refresh_token", refresh_token);
 
-		const res = (<any>request).res as express.Response;
-		const token_response = await fetch("https://discord.com/api/oauth2/token", {
+		const res = (<any>request).res as ExResponse;
+		const tokenResponse = await fetch("https://discord.com/api/oauth2/token", {
 			method: "POST",
 			body: params,
 		});
-		const json: any = await token_response.json();
+		const json: any = await tokenResponse.json();
 
 		if ("error" in json) {
 			res.status(500).json(json).end();
@@ -96,15 +75,15 @@ export class AuthController extends Controller {
 	 * @param target Redirect target app
 	 */
 	@Get("discord/{target}")
-	public discordAuthGrant(@Path() target: string, @Request() request: express.Request) {
-		const redirect_uri = this.shapeRedirectURI(request);
+	public discordAuthGrant(@Path() target: string, @Request() request: ExRequest) {
+		const redirectURI = this.service.getRedirectURI(request);
 
-		const response = (<any>request).res as express.Response;
+		const response = (<any>request).res as ExResponse;
 		response.redirect(
 			`https://discord.com/api/oauth2/authorize` +
 				`?client_id=${process.env.DISCORD_CLIENT_ID}` +
 				`&response_type=code&scope=identify` +
-				`&redirect_uri=${redirect_uri}` +
+				`&redirect_uri=${redirectURI}` +
 				`&state=${target}` +
 				``,
 		);
---
+++ src/v2/service/auth.service.ts
@@ -0,0 +1,25 @@
+import { Request as ExRequest } from "express";
+
+// REDIRECT URL IS '<origin>/auth/<provider>/callback'
+const REDIRECT_URI_PATHNAME = "/callback";
+// example with discord provider:
+// REDIRECT URL IS '<origin>/auth/discord/callback'
+
+export default class AuthService {
+	rawTargetToURL(target: string) {
+		if (target === "webapp") return process.env.WEBAPP_URL;
+		return "";
+	}
+
+	targetToURL(target: string) {
+		return this.rawTargetToURL(target).replace(/\/$/, "");
+	}
+
+	getRedirectURI(request: ExRequest) {
+		const apiOrigin = `${request.protocol}://${request.headers.host}`;
+		const requestPath = request.path;
+		const pathSplit = requestPath.split("/");
+		pathSplit.pop(); // remove target
+		return apiOrigin.replace(/\/$/, "") + pathSplit.join("/") + REDIRECT_URI_PATHNAME;
+	}
+}
